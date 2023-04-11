62127e8d8ebfe22d08d3a848247bf5fbed0adcc7
MeeWbrh <bawar@live.dk>
2023-04-11 02:15:08 +0200

    changes in WCScreen + testing tailwindcss

--- src/screens/WelcomeScreen.tsx
+++ src/screens/WelcomeScreen.tsx
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import {
   IonCard,
   IonCardContent,
@@ -13,17 +12,26 @@ const WelcomeScreen: React.FC = () => {
   return (
     <IonPage>
       <IonContent>
-        <IonCard className="mt-20">
+        <IonCard className="mt-10 rounded-lg">
           <IonCardHeader>
-            <IonCardTitle className=" text-cyan-300">Card Title</IonCardTitle>
-            <IonCardSubtitle className="font-bold text-red-400">
+            <IonCardTitle className="text-cyan-300 flex justify-center">
+              Card Title
+            </IonCardTitle>
+            <IonCardSubtitle className="font-bold text-red-400 flex justify-center">
               Card Subtitle
             </IonCardSubtitle>
           </IonCardHeader>
-
-          <IonCardContent className="font-extrabold text-green-400">
-            Here's a small text description for the card content. Nothing more,
-            nothing less.
+          <IonCardContent>
+            <div className=" text-green-400 flex justify-center font-extrabold">
+              Here's a small text in the content section
+            </div>
+            <div className="mt-5 mb-5 flex justify-center">
+              <img
+                src="https://play-lh.googleusercontent.com/ShEdBTR84fzjVgBMzEHKDAGcbfEtyFUlO1jv0W2-viKsx2CvphvrfKAeQft1PCu8VUw"
+                alt="img1"
+                className="h-[12.5rem] w-[12.5rem] rounded-full"
+              />
+            </div>
           </IonCardContent>
         </IonCard>
       </IonContent>
