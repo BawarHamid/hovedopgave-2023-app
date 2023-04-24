edf7c8b6f261a56a3494d1c2b66d30fc5f40868d
MeeWKurt <105715024+Gamblefish@users.noreply.github.com>
2023-04-24 11:22:29 +0200

    login screen functionality done

--- src/screens/authentication/LoginScreen.tsx
+++ src/screens/authentication/LoginScreen.tsx
@@ -5,7 +5,7 @@ import {
   useIonLoading,
   useIonRouter,
 } from "@ionic/react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useHistory } from "react-router";
 import AuthHeader from "../../components/generic/headers/auth-header/AuthHeader";
 import { chevronBack } from "ionicons/icons";
@@ -13,13 +13,49 @@ import AuthInput, {
   AuthInputType,
 } from "../../components/authentication/auth-input/AuthInput";
 import RegularButton from "../../components/generic/styled-regulars/button/RegularButton";
+import { checkUserHasProfile } from "../../apis/supabase/profile";
+import { supabase } from "../../apis/supabase/supabaseClient";
+import { useAuthUserStore } from "../../store/user";
 
 const LoginScreen: React.FC = () => {
   const history = useHistory();
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [isPasswordRevealed, setIsPasswordRevealed] = useState<boolean>(false);
+  const [isSubmitDisabled, setIsSubmitDisabled] = useState<boolean>(true);
+
   const router = useIonRouter();
+  const [present, dismiss] = useIonLoading();
+  const [presentAlert] = useIonAlert();
+
+  const setAuthUser = useAuthUserStore((state) => state.setAuthUser);
+
+  useEffect(() => {
+    setIsSubmitDisabled(!(email.includes("@") && password !== ""));
+  }, [email, password]);
+
+  const handleLogin = async (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    await present({ message: "Signing in..." });
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (data.user && data.user.aud === "authenticated") {
+      const hasProfile = await checkUserHasProfile(data.user.id);
+      setAuthUser(data.user);
+      await dismiss();
+      hasProfile ? router.push("/home") : router.push("/onboarding");
+    } else {
+      await dismiss();
+      await presentAlert({
+        header: "Login failed",
+        message: error?.message,
+        buttons: ["OK"],
+      });
+    }
+  };
 
   return (
     <IonPage>
@@ -50,7 +86,12 @@ const LoginScreen: React.FC = () => {
               setShown={setIsPasswordRevealed}
             />
 
-            <RegularButton text="Login" rounded />
+            <RegularButton
+              text="Login"
+              rounded
+              onClick={handleLogin}
+              disabled={isSubmitDisabled}
+            />
 
             <div className="w-full flex justify-end mt-2">
               <h5
